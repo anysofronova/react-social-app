21f5033e16fb45bd62d1728eb9338267a1c12e3a
Anna <anysofronova@yandex.ru>
2022-03-30 11:01:53 +0300

    Dialogs component and scss

--- react-social-app/src/components/dialogs/Dialogs.jsx
+++ react-social-app/src/components/dialogs/Dialogs.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./dialogs.scss";
+import { Link } from "react-router-dom";
 
 function Dialogs() {
-  return <div>Dialogs</div>;
+  return (
+    <div className="dialogs">
+      <div className="dialogs__wrapper">
+        <div className="dialogs__list">
+          <div className="dialogs__item">
+            <Link to="/dialogs/1">Anna</Link>
+          </div>
+          <div className="dialogs__item">
+            <Link to="/dialogs/2">Vlad</Link>
+          </div>
+          <div className="dialogs__item">
+            <Link to="/dialogs/3">Misha</Link>
+          </div>
+          <div className="dialogs__item">
+            <Link to="/dialogs/4">Sergey</Link>
+          </div>
+        </div>
+        <div className="dialogs__messages">
+          <div className="dialogs__message">Hi</div>
+          <div className="dialogs__message">How are you?</div>
+          <div className="dialogs__message">What's up?</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
