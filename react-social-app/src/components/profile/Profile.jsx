da3b51139fcb65f48dfd553c5a26d8105014f54e
Anna <anysofronova@yandex.ru>
2022-03-30 12:50:13 +0300

    SCSS

--- react-social-app/src/components/profile/Profile.jsx
+++ react-social-app/src/components/profile/Profile.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "./profile.scss";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./profileInfo/ProfileInfo";
 
 function Profile() {
   return (
     <div className="profile">
       <div className="profile__wrapper">
-        <div className="profile__img">
-          <img
-            src="https://img1.goodfon.ru/original/1920x1200/8/52/emma-uotson-emma-watson-1608.jpg"
-            alt=""
-          />
-        </div>
-        <div className="profile__desc">ava + desc</div>
+        <ProfileInfo />
         <MyPosts />
       </div>
     </div>
