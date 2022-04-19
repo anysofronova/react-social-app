7ff8e6c0940c82e0f75cd15bf68c463669c1d144
Anna <anysofronova@yandex.ru>
2022-04-19 09:14:43 +0300

    Tests

---
+++ react-social-app/src/redux/profileReducer.test.js
@@ -0,0 +1,38 @@
+import profileReducer, { addPost } from "./profileReducer";
+
+test("length of posts should be incremented", () => {
+  let action = addPost("Hey there.");
+  let state = {
+    postsData: [
+      {
+        id: 1,
+        post: "С другой стороны укрепление и развитие структуры обеспечивает участие в формировании систем массового участия.",
+        likesCount: 1023,
+      },
+      {
+        id: 2,
+        post: "Если у вас есть какие то интересные предложения, обращайтесь! Студия Web-Boss всегда готова решить любую задачу.",
+        likesCount: 319,
+      },
+      {
+        id: 3,
+        post: "Повседневная практика показывает, что реализация намеченных плановых заданий в значительной степени обуславливает создание модели развития.",
+        likesCount: 238,
+      },
+      {
+        id: 4,
+        post: "С другой стороны рамки и место обучения кадров способствует подготовки и реализации модели развития.",
+        likesCount: 194,
+      },
+      {
+        id: 5,
+        post: "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке модели развития.",
+        likesCount: 739,
+      },
+    ],
+    newPostText: "",
+  };
+  let newState = profileReducer(state, action);
+  expect(newState.postsData.length).toBe(6);
+  // expect(newState.postsData[0].post).toBe("Hey there.");
+});
